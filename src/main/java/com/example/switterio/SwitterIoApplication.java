ee658e787709188b07a9b02058fb96df2ae2e305
Uladislav Bialou <104993048+Belka141@users.noreply.github.com>
2022-07-27 16:06:24 +0300

    Add files via upload

---
+++ src/main/java/com/example/switterio/SwitterIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.switterio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitterIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitterIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/switterio/SwitterIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.switterio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitterIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
