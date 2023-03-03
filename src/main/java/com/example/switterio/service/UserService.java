2ce9f5ce703c5f32cf9002261a910fd0567e384b
Uladislav Bialou <belo4kalala1441@gmail.com>
2023-03-03 11:40:44 +0200

    prop

--- src/main/java/com/example/switterio/service/UserService.java
+++ src/main/java/com/example/switterio/service/UserService.java
@@ -50,7 +50,7 @@ public class UserService implements UserDetailsService {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
-                            "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                            "Welcome to SwitterIO. Please, visit next link: http://localhost:8080/activate/%s",
                     user.getUsername(),
                     user.getActivationCode()
             );
